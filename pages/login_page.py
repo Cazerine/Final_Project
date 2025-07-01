d09e6a965e259099f73e67f4f23496679a52c3f8
Catherine <hofmanncaz@yandex.ru>
2025-07-01 17:52:17 +0300

    added files login_page.py and locators.py. modified main_page (changed locator) and test_main_page.py (added LoginPage)

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .main_page import MainPage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        login_url = self.browser.current_url
+        assert "login" in login_url, "wrong login_link"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'there is no login_form'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), 'there is no registration_form'
